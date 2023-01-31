98b3775fc529addc9ad6a2a044be2349c34b43ba
giraffeguids@gmail.com <giraffeguids@gmail.com>
2023-01-31 13:00:35 +0600

    rashid pidor

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿//
+//
+//
+Console.WriteLine("Сколько раз выполнить цикл");
+int length = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= length; i++)
+{
+    for (int b = 0; b < i ; b++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = length; i > 0; i--)
+{
+    for (int ii = i; ii > 0; ii--)
+    {
+        Console.Write("#");
+    }
+    
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Конец цикла");
+Console.ReadLine();
---
+++ ConsoleApp2/calc.cs
@@ -0,0 +1,5 @@
+﻿//
+//
+int[] array = new int[10];
+array[3] = 3;
+Console.WriteLine(array[3]);
